082300644d2692da37aea2ac8abe6b313448e1ab
Esmaeil Ahmadipour <software8686@gmail.com>
2020-02-05 04:54:00 +0330

    Commit> TeamDao> Add Body Of Save Method

--- app/src/main/java/ir/ea2/kotlinsqlite/data/local/db/dao/BaseDao.kt
+++ app/src/main/java/ir/ea2/kotlinsqlite/data/local/db/dao/BaseDao.kt
@@ -1,8 +1,10 @@
 package ir.ea2.kotlinsqlite.data.local.db.dao
 
+import android.content.ContentValues
 import ir.ea2.kotlinsqlite.data.local.db.AppDatabase
+abstract class BaseDao<T>(val appDatabase: AppDatabase) {
+    val contentValue = ContentValues()
 
-abstract class BaseDao<T>(appdatabase: AppDatabase) {
     abstract fun  save (entity:T):Boolean
     abstract  fun save (id:String , entity: T):Boolean
     abstract fun findAll():List<T>
--- app/src/main/java/ir/ea2/kotlinsqlite/data/local/db/dao/TeamDao.kt
+++ app/src/main/java/ir/ea2/kotlinsqlite/data/local/db/dao/TeamDao.kt
@@ -3,9 +3,18 @@ package ir.ea2.kotlinsqlite.data.local.db.dao
 import ir.ea2.kotlinsqlite.data.local.db.AppDatabase
 import ir.ea2.kotlinsqlite.data.model.Team
 
+//Three Action is important. 1-open database/2-save database/3-close database
 class TeamDao(appDatabase: AppDatabase) : BaseDao<Team>(appDatabase) {
     override fun save(entity: Team): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val db = appDatabase.writableDatabase
+        contentValue.clear()
+        contentValue.put(AppDatabase.TEAM_NAME, entity.name)
+        contentValue.put(AppDatabase.TEAM_GROUND, entity.ground)
+        val insertResult = db.insert(AppDatabase.TEAM_TABLE, null, contentValue)
+        db.close()
+      if(insertResult>0) return true
+        return false
+
     }
 
     override fun save(id: String, entity: Team): Boolean {
